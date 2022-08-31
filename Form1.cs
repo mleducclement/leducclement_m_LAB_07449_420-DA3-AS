972346784100cacc44fc9d0fbac0e53efbca5fe5
Michael <michael.leducclement@gmail.com>
2022-08-31 12:28:24 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace leducclement_m_LAB_07449_420_DA3_AS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
